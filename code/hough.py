7faf3c812ab0d8bdb35e65191116715466cb08b3
Shane Grouse <shane.grouse3@mail.dcu.ie>
2020-03-01 17:01:05 +0000

    Final confidnet search. Also added canny function to see the copy of the image before the circle search

--- code/hough.py
+++ code/hough.py
@@ -9,7 +9,7 @@ def show(image, title="Default"):
     cv2.destroyWindow(title)
     return key
 
-def detectFingers(filename):
+def detectFingers(filename, thres=180):
     img = cv2.imread(filename)
     # show(img, "original")
 
@@ -17,52 +17,56 @@ def detectFingers(filename):
     copy = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY) # Make the copy greyscale
     # show(copy, "grey")
 
-    # copy = cv2.medianBlur(copy, 9)
+    copy = cv2.medianBlur(copy, 15)
     # show(copy, "blur")
 
+    # Used to see result of Canny algorithm
+    # hough.Circle uses a given number as the higher threshold, and divides by 2 for the lower
+    canny = cv2.Canny(copy, thres/2, thres) 
+    # show(canny, "edge")
     """
     cv.Hough Circles
     Function to find circles in images. Requires 8-bit, grayscale images
 
     Some parameters:
     dp is the accumulator resolution ratio. Keep at 1 for co-ords to be used with original image
-    minDist is the minimum distance between circles
+    minDist is the minimum distance between circles centers
     param1 is edge detection threshold. Higher value for more defined edges. See cv.Canny
-    param2 is circle accumulator threshold. Lower values accept more circles 
+    param2 is circle accumulator threshold. Lower values accept more circles
     """  
-    
-    # Temporary values here
-    # minDist set to 20
-    # This has no blurring and high edge detection. Trying to get the detail from fingernails
-    circles = cv2.HoughCircles(copy, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=80, param2=25, minRadius=0, maxRadius=40)
+    # This should be the "conservative" search
+    # minDist = 25, param1 = 180, param = 20
+    circles = cv2.HoughCircles(copy, cv2.HOUGH_GRADIENT, dp=1, minDist=25, param1=thres, param2=20, minRadius=0, maxRadius=40)
     circles = np.uint16(np.around(circles))
 
     for (x, y, r) in circles[0, :]:
-        if r <= 35:
+        if y >= 150:
             cv2.circle(img, (x, y), r, (0, 255, 0), 3)
         else:
             cv2.circle(img, (x, y), r, (0, 0, 255), 3)
         
         cv2.circle(img, (x, y), 2, (255, 0, 0), 2)
 
+    # cv2.circle(img, (320, 240), 2, (255, 255, 0), 2) # center of img
     key = show(img, "{} of {}: {}".format(i, total, filename))
     return key
 
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        folder = sys.argv[1]
+    else:
+        folder = "images"
 
-if len(sys.argv) > 1:
-    folder = sys.argv[1]
-else:
-    folder = "images"
+    os.chdir(folder)
+    i = 0
+    files = os.listdir()
+    total = len(files)
+    for file in files:
+        key = detectFingers(file, 180)
+        # key = lines(file)
+        
+        if key == 113:
+            break
 
-os.chdir(folder)
-i = 0
-files = os.listdir()
-total = len(files)
-for file in files:
-    key = detectFingers(file)
-    
-    if key == 113:
-        break
-
-    i += 1
+        i += 1
